149dc63db02b6db91797de956047dfabd274618b
Jesse Lord <jesse.al.lord@gmail.com>
2023-09-25 14:34:39 -0400

    Add event handler for navbar clicks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import { buildPageOne } from "./page1";
 
-
 const content = document.querySelector('#content');
+const navItems = document.querySelectorAll('.nav-item')
 buildPageOne(content);
+
+const page = (function () {
+    function navbarClickHandler() {
+        clearContainer(content);
+    }
+
+    function clearContainer(container) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+
+    //Set event listener on every nav link element that isn't active
+    navItems.forEach(element => {
+        element.removeEventListener("click", navbarClickHandler);
+    })
+    Array.from(navItems).filter((element) => !element.classList.contains("active-nav")).forEach(element => {
+        element.addEventListener("click", navbarClickHandler);
+    });
+
+
+    return ({ navbarClickHandler, clearContainer })
+})();
